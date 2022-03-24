162f4b948785ac9e35f13951fc686ca297cdbcea
Dipon Sarker <dipon778@gmail.com>
2022-03-24 13:30:25 +0600

    Place , Date Number of person selection is added and can search anything

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from booking.booking import Booking
 
 with Booking() as bot:
     bot.land_first_page()
-    # bot.change_currency(currency='EUR')
+    bot.change_currency(currency='EUR')
     bot.select_place(place='New York')
     bot.select_dates(ch_in="2022-03-25",ch_out="2022-03-28")
     bot.select_adults(adults=4)
